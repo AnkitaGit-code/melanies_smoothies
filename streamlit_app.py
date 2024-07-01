f34fb05345695087c54536cc6fec247ce13464b3
AnkitaGit-code <swapnachaple123@gmail.com>
2024-07-01 11:53:07 +0530

    Update streamlit_app.py
    ad

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutritional Information')
@@ -36,13 +36,7 @@ if ingredients_list:
             values ('""" + ingredients_string +"""','""" + NAME_ON_ORDER + """')"""
 
     st.write(my_insert_stmt)
-    #st.stop()
     time_to_insert = st.button ('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-#fv.df=st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
