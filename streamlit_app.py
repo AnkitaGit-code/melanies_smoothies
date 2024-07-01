67d8d605f8234c3a6602abe88663483821c70c2d
AnkitaGit-code <swapnachaple123@gmail.com>
2024-07-01 12:00:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,14 @@ ingredients_list = st.multiselect(
     "Choose Upto 5 Ingredients",my_dataframe, max_selections=5
 )
 
+import requests
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutritional Information')
-        import requests
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
